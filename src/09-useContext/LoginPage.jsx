cd5abb5fc97cb15bd6f828c2245ec0cbfd80e167
mundoenrique <mundoenrique@hotmail.com>
2022-06-28 18:37:35 -0400

    Implenta useContext

--- src/09-useContext/LoginPage.jsx
+++ src/09-useContext/LoginPage.jsx
@@ -1,7 +1,25 @@
+import { useContext } from 'react';
+import { UserContext } from './context/UserContext';
+
 export function LoginPage() {
+	const { user, setUser } = useContext(UserContext);
+
 	return (
 		<>
 			<h1>LoginPage</h1>
+			<button
+				className="btn btn-outline-primary mt-2 mb-2"
+				onClick={() =>
+					setUser({
+						id: 123456,
+						name: 'Enrique Peñaloza',
+						email: 'enrique@gmail.com',
+					})
+				}
+			>
+				Set User
+			</button>
+			<pre>{JSON.stringify(user, null, 3)}</pre>
 		</>
 	);
 }
--- src/09-useContext/MainApp.jsx
+++ src/09-useContext/MainApp.jsx
@@ -1,12 +1,13 @@
 import { Route, Routes } from 'react-router-dom';
 import { AboutPage } from './AboutPage';
+import { UserProvider } from './context/UserProvider';
 import { HomePage } from './HomePage';
 import { LoginPage } from './LoginPage';
 import { NavBar } from './NavBar';
 
 export default function MainApp() {
 	return (
-		<>
+		<UserProvider>
 			<NavBar />
 			<Routes>
 				<Route path="/" element={<HomePage />} />
@@ -15,6 +16,6 @@ export default function MainApp() {
 				<Route path="*" element={<h1>No found</h1>} />
 				{/* <Route path="*" element={<Navigate to="/" />} /> */}
 			</Routes>
-		</>
+		</UserProvider>
 	);
 }
