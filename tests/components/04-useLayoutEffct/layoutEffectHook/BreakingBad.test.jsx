eebe941469e07603d8042ca669849b82ee9d110c
mundoenrique <mundoenrique@hotmail.com>
2022-06-30 16:40:46 -0400

    Pruebas en <BreakingBad />

---
+++ tests/components/04-useLayoutEffct/layoutEffectHook/BreakingBad.test.jsx
@@ -0,0 +1,59 @@
+import { render, screen, fireEvent } from '@testing-library/react';
+import { BreakingBad } from '../../../../src/components/04-useLayoutEffct';
+import { useCounter } from '../../../../src/hooks/useCounter';
+import { useFetch } from '../../../../src/hooks/useFetch';
+
+jest.mock('../../../../src/hooks/useCounter');
+jest.mock('../../../../src/hooks/useFetch');
+
+describe('Pruebas en <BreakingBad />', () => {
+	const mockIncrement = jest.fn();
+	const quoteInfo = { author: 'An author', quote: 'A quote' };
+
+	useCounter.mockReturnValue({
+		counter: 1,
+		increment: mockIncrement,
+	});
+
+	useFetch.mockReturnValue({
+		data: null,
+		isLoading: true,
+		hasError: null,
+	});
+
+	beforeEach(() => {
+		jest.clearAllMocks();
+	});
+
+	test('Debe mostrar el componente por defecto', () => {
+		render(<BreakingBad />);
+		const nextButton = screen.getByRole('button', { name: 'Next Quote' });
+
+		expect(screen.getByText('BreakingBad'));
+		expect(screen.getByText('Loading...'));
+		expect(screen.getByText('Loading...'));
+		expect(nextButton.disabled).toBeTruthy();
+	});
+
+	test('Debe mostrar un quote', () => {
+		useFetch.mockReturnValue({
+			data: [quoteInfo],
+			isLoading: false,
+			hasError: null,
+		});
+		render(<BreakingBad />);
+		const nextButton = screen.getByRole('button', { name: 'Next Quote' });
+
+		expect(screen.getByText(quoteInfo.author)).toBeTruthy();
+		expect(screen.getByText(quoteInfo.quote)).toBeTruthy();
+		expect(nextButton.disabled).toBeFalsy();
+	});
+
+	test('Debe llamar a la función de incrementar', () => {
+		render(<BreakingBad />);
+		const nextButton = screen.getByRole('button', { name: 'Next Quote' });
+		fireEvent.click(nextButton);
+
+		expect(mockIncrement).toHaveBeenCalledTimes(1);
+	});
+});
